4693a6f7d0e096b8a4c14b807304ceba3a451f16
王璇 <wangxuan2022@mail.nwpu.edu.cn>
2025-09-03 20:29:26 +0800

    。

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 # 确保文件夹是空的
 # npm i hexo-theme-stellar
 hexo init
+
+
+
+git clone https://github.com/shaoyaoqian/hexo-theme-stellar.git themes/stellar
+cd themes/stellar
+git checkout main-xaoxuu-129
+cd ../..
+npm install hexo
+npm install -g hexo-cli
+hexo serve
